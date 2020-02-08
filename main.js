8963ce7e07bdbca10faffe6c85e6c355cb0693f0
Derek McDaniel <derek7mc@gmail.com>
2020-02-08 16:52:42 -0700

    config and help commands work.

--- main.js
+++ main.js
@@ -3,15 +3,70 @@ let http = require('http');
 let fs = require('fs');
 let qs = require('querystring');
 let template = require('lodash')._.template;
+let helpMessages = {};
+let commands = {};
+let apps = {};
 
 let cmd_page = template(fs.readFileSync('./views/cmd_page.html'));
 const NEW_CONTEXT = "\n==NEW CONTEXT==\n";
+const config_regex = /^[A-Za-z0-9 ]*$/;
 
 // config is a whitespace separated list of tuples
-const DEFAULT_CONFIG = parseConfig(
-    "rowcount 19 colcount 54 darkmode 0");
+const DEFAULT_CONFIG = "rowcount 19 colcount 54 darkmode false";
+const DEFAULT_HIST = "help";
+
+// Commands are just apps that operate on the global state context,
+// and not a localized state.
+
+// TODO loadCommand and loadApp functions load apps or commands from a file and set their help messages.
+// TODO a DataApp uses database callbacks like revision-db.
+function loadCmd(filename){
+}
+function loadApp(filename){
+}
+function loadDataApp(filename){
+}
+
+helpMessages.config =
+  "Edit config.\n" + 
+  " Example: \"config darkmode true\"\n" +
+  " Type 'config' with no arguments to see current config.\n";
+
+commands.config = function(args, puts, state){
+    if(args.length == 1){
+        puts(" Configuration: " + dumpConfig(state.config));
+        return null; }
+    let config_str = args.slice(1).join(" ");
+    if(!config_regex.test(config_str)){
+        puts(" 'config' only allows alphanumeric values.");
+        return; }
+    let change_config = parseConfig(config_str);
+    state.config = mergeMap(state.config, change_config);
+    puts(" config changes: " + dumpConfig(change_config));
+    return state;
+}
+
+helpMessages.help = "Show help for a command. Example: \"help config\".";
+
+commands.help = function(args, puts, state){
+    if(args.length == 1){
+        args.push('help'); }
+    for(var i=1; i<args.length; ++i){
+        let cmd = args[i];
+        if(cmd in helpMessages){
+            puts(' ' + cmd + ": " + helpMessages[cmd]); }
+        else{
+            puts(" No help for '" + cmd + "'."); }
+    }
+}
+
 
 // second map overwrites first.
+function mergeIntoMap(result, a){
+    for(let k in a){
+        result[k] = a[k]; }
+    return result;
+}
 function mergeMap(a, b){
     let result = {};
     for(let k in a){
@@ -58,7 +113,6 @@ function parseHist(hist_str){
 
 function dumpHist(hist){
     let result_array = [];
-    // console.log('hist', hist);
     if(!hist) return "";
     for(let i=0; i<hist.length; ++i){
         // console.log('hist[i]', hist[i]);
@@ -69,25 +123,22 @@ function dumpHist(hist){
 
 let server = http.createServer(function(request, response){
     let data  = {
-        "title": "This is the title!!!!!",
-        "config" : dumpConfig(DEFAULT_CONFIG),
-        "cmd_out" : "buffer data",
-        "cmd_hist" : "help",
+        "title": "cmd",
+        "config" : parseConfig(DEFAULT_CONFIG),
+        "cmd_out" : "Type 'help' for help.",
+        "cmd_hist" : parseHist(DEFAULT_HIST),
+        "app_name" : "",
+        "app_state" : "",
     }
-    let config = parseConfig(data.config);
-    let cmd_out = "";
-    let cmd_hist = parseHist(data.cmd_hist); // TODO a stack of cmd_hist contexts.
-    // console.log('cmd_hist', cmd_hist);
-    // console.log('config', config);
 
     if(request.method == "GET"){
         response.writeHead(200, {"Content-Type": "text/html"});
-        mainPage();
+        cmdPage(data);
     }
     if(request.method == "POST"){
         let requestBody = '';
-        request.on('data', function(data){
-            requestBody += data;
+        request.on('data', function(_data){
+            requestBody += _data;
 			if(requestBody.length > 1e7) {
 			    response.writeHead(413, 'Request Entity Too Large', {'Content-Type': 'text/html'});
 			    response.end('<!doctype html><html><head><title>413</title></head><body>413: Request Entity Too Large</body></html>');
@@ -99,63 +150,78 @@ let server = http.createServer(function(request, response){
             // handle form data to modify local variables and 
             if(formData.config){
                 let post_config = parseConfig(config_str);
-                config = mergeMap(config, post_config);
+                data.config = mergeMap(data.config, post_config);
             }
             if(formData.cmd_out){
-                cmd_out = formData.cmd_out;
+                data.cmd_out = formData.cmd_out;
             }
             if(formData.cmd_hist){
-                cmd_hist = parseHist(formData.cmd_hist);
+                data.cmd_hist = parseHist(formData.cmd_hist);
+            }
+            // TODO do we want an app stack?
+            if(formData.app_name){
+                data.app_name = formData.app_name;
+            }
+            if(formData.app_state){
+                data.app_state = formData.app_state;
             }
             if(formData.cmd_text){
-                cmd_text = formData.cmd_text;
-                if(cmd_text.length){
-                    if(!cmd_hist){
-                        cmd_hist = [[]]; }
-                    if(!cmd_hist.length){
-                        cmd_hist.push([]); }
-                    cmd_hist[cmd_hist.length - 1].push(cmd_text);
+                data.cmd_text = formData.cmd_text;
+                if(data.cmd_text.length){
+                    if(!data.cmd_hist){
+                        data.cmd_hist = [[]]; }
+                    if(!data.cmd_hist.length){
+                        data.cmd_hist.push([]); }
+                    data.cmd_hist[data.cmd_hist.length - 1].push(data.cmd_text);
                 }
             }
 
-            // TODO replace test values with command callback processing.
-            cmd_out += `\n> ${cmd_text}\n TODO process commands.`;
-            // if(cmd_hist && cmd_hist.length > 0){
-            //    cmd_hist[cmd_hist.length - 1].push( "demo-test-command added to history for testing.");
-            // }
+            let puts = function(s){ data.cmd_out += "\n" + s; }
+            if(data.app_name.length){
+                data.app_state = handleApp(data.app_name, data.cmd_text, puts, data.app_state); }
+            else{
+                handleCommand(data.cmd_text, puts, data); }
 
-            // console.log('config', config);
-            // console.log('data', data);
-            mainPage();
+            cmdPage(data);
         });
     }
+    function handleApp(app_name, cmd_text, puts, state){
+        // TODO
+    }
+    function handleCommand(cmd_text, puts, data){
+        puts("> " + cmd_text);
+        let args = cmd_text.split(/\s+/);
+        if(!args.length) return;
+        let cmd = args[0];
+        if(cmd in commands){
+            commands[cmd](args, puts, data); }
+        else{
+            puts(" Unknown command: '" + cmd + "'"); }
+    }
     // closure for rendering the page.
-    function mainPage(){
-        data.cmd_out = cmd_out;
-        data.cmd_hist = dumpHist(cmd_hist);
-        data.config = dumpConfig(config);
+    function cmdPage(data){
+        let template_data = {};
+
+        template_data.title = data.title;
+        template_data.cmd_out = data.cmd_out.split("\n").slice(0, data.config.rowcount-1).join("\n");
+        // console.log('cmd_hist', data.cmd_hist);
+        template_data.cmd_hist = dumpHist(data.cmd_hist);
+        template_data.config = dumpConfig(data.config);
 
         // add config vars to rendering data context.
-        for(let key in DEFAULT_CONFIG){
-            if(key in config){
-                data[key] = config[key]; }
-            else{
-                data[key] = DEFAULT_CONFIG[key]; }
+        for(let key in data.config){
+            template_data[key] = data.config[key];
         }
 
-        if(config.darkmode){
-            data.background = '#000';
-            data.foreground = '#fff';
-        } else {
-            data.background = '#fff';
-            data.foreground = '#000';
-        }
+        let dark = data.config.darkmode === "true";
+        template_data.background = dark? '000' : 'fff';
+        template_data.foreground = dark? 'fff' : '000';
 
         response.writeHead(200, {"Content-Type": "text/html"});
-        let html = cmd_page(data);
+        let html = cmd_page(template_data);
         response.end(html);
     }
 })
 server.listen(8000);
 
-console.log("Server running on port 8000");
+console.log("Server running on port 8000.");
